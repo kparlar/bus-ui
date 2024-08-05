ab1a5acc167fdc4f2c9d023d076456f44e1d7386
koray <korayparlar@hotmail.com>
2024-08-05 12:35:33 +0300

    added header component added home component implemented initial phase of pages

---
+++ src/app/component/bus-header/bus-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bus-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './bus-header.component.html',
+  styleUrl: './bus-header.component.scss'
+})
+export class BusHeaderComponent {
+
+}
